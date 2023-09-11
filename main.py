29c769c6cd0cefdfe7453c69d220458bf4c1100c
Jhin <47354855+Jhinpan@users.noreply.github.com>
2023-09-11 12:25:19 -0400

    Regex Fix

--- main.py
+++ main.py
@@ -2,37 +2,42 @@ import os
 import re
 
 
-def extract_prefab_data(file_path):
-    # Regular expressions to extract necessary details
-    gameobject_name_pattern = re.compile(r'm_Name: (\w+)')
-    local_position_pattern = re.compile(r'm_LocalPosition: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*)}')
-    local_rotation_pattern = re.compile(
-        r'm_LocalRotation: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*), w: (-?\d+\.?\d*)')
-    local_scale_pattern = re.compile(r'm_LocalScale: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*)}')
-
-    # Exclude patterns
-    exclude_patterns = ["ObjectHighlighter", "Environment_", "BFW180201_", "solid", "glass_", "Wall", "xWalls", "zWalls", "Rooms", "RoomTypes", "Bedroom1", "Bedroom2", "livingroom",
-                        "Window", "Environment", "Floor", "CreatedObjects", "CreatedBoxes", "BoxGroup", "visuals", "link_", "midpoint_",
+def extract_prefab_data_chunked(file_path):
+    exclude_patterns = ["ObjectHighlighter", "Environment_", "BFW180201_", "solid", "glass_", "Wall", "xWalls",
+                        "zWalls", "Rooms", "RoomTypes", "Bedroom1", "Bedroom2", "livingroom",
+                        "Window", "Environment", "Floor", "CreatedObjects", "CreatedBoxes", "BoxGroup", "visuals",
+                        "link_", "midpoint_",
                         "corner_", "faceCenter_", "shadow", "Black_Right_Arrow", "rigRoot"]
-
 
     with open(file_path, 'r') as prefab_file:
         content = prefab_file.read()
 
-    gameobject_names = gameobject_name_pattern.findall(content)
-    local_positions = local_position_pattern.findall(content)
-    local_rotations = local_rotation_pattern.findall(content)
-    local_scales = local_scale_pattern.findall(content)
+    # Splitting the content based on "\nGameObject"
+    gameobject_chunks = content.split("\nGameObject")[1:]
 
-    data = list(zip(gameobject_names, local_positions, local_rotations, local_scales))
+    # Patterns for extraction
+    gameobject_name_pattern = re.compile(r'm_Name: (\w+)')
+    local_position_pattern = re.compile(r'm_LocalPosition: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*)}')
+    local_rotation_pattern = re.compile(
+        r'm_LocalRotation: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*), w: (-?\d+\.?\d*)}')
+    local_scale_pattern = re.compile(r'm_LocalScale: {x: (-?\d+\.?\d*), y: (-?\d+\.?\d*), z: (-?\d+\.?\d*)}')
 
-    # Filter out the unwanted data
-    filtered_data = [item for item in data if not any(pattern in item[0] for pattern in exclude_patterns)]
+    extracted_data = []
+    for chunk in gameobject_chunks:
+        name = gameobject_name_pattern.search(chunk)
+        position = local_position_pattern.search(chunk)
+        rotation = local_rotation_pattern.search(chunk)
+        scale = local_scale_pattern.search(chunk)
+
+        if name and position and rotation and scale:
+            extracted_data.append((name.group(1), position.groups(), rotation.groups(), scale.groups()))
+
+    filtered_data = [item for item in extracted_data if not any(pattern in item[0] for pattern in exclude_patterns)]
 
     return filtered_data
 
 
-def parse_directory_for_prefabs(root_directory):
+def parse_directory_for_prefabs_updated(root_directory):
     all_prefab_data = {}
 
     # Loop through P1 to P14 folders
@@ -56,7 +61,7 @@ def parse_directory_for_prefabs(root_directory):
                         if file.startswith("Environment_") and file.endswith(".prefab"):
                             full_path = os.path.join(c_folder, file)
                             e_number = file.split('_')[-1].split('.')[0]  # Extracting the number after "Environment_"
-                            all_prefab_data[f'P{p_index}'][f'C{c_index}'][f'E{e_number}'] = extract_prefab_data(
+                            all_prefab_data[f'P{p_index}'][f'C{c_index}'][f'E{e_number}'] = extract_prefab_data_chunked(
                                 full_path)
 
     return all_prefab_data
@@ -86,5 +91,5 @@ if not os.path.exists(save_directory):
     os.makedirs(save_directory)
 
 # Run the script and save results
-all_data = parse_directory_for_prefabs(root_directory)
+all_data = parse_directory_for_prefabs_updated(root_directory)
 save_data_to_files(all_data, save_directory)
